ba46d0a9907f32e36889eafed0790a22f95d5c5a
biswajit-github-2022 <biswajitranaplanet@gmail.com>
2023-07-14 22:32:54 +0530

    sec

--- KNN.py
+++ KNN.py
@@ -1,39 +1,40 @@
-import pandas as pd 
-import numpy as np 
-import matplotlib.pyplot as plt 
-from sklearn.preprocessing import StandardScaler 
-from sklearn.model_selection import train_test_split 
-from sklearn.neighbors import KNeighborsClassifier 
-from sklearn.metrics import classification_report, confusion_matrix 
-df = pd.read_csv("prostate2.csv") 
-scaler = StandardScaler() 
-scaler.fit(df.drop('target', axis=1)) 
-scaled_features = scaler.transform(df.drop('target', axis=1)) 
-df_feat = pd.DataFrame(scaled_features, columns=df.columns[:-1]) 
-X_train, X_test, y_train, y_test = train_test_split(scaled_features, df['target'], test_size=0.30) 
-knn = KNeighborsClassifier(n_neighbors=1) 
-knn.fit(X_train, y_train) 
+# Import necessary modules
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.datasets import load_iris
+import numpy as np
+import matplotlib.pyplot as plt
+
+irisData = load_iris()
+
+# Create feature and target arrays
+X = irisData.data
+y = irisData.target
+print(len(X))
+# Split into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42)
+print(X_test,y_test)
+neighbors = np.arange(1, 9)
+train_accuracy = np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
+
+# Loop over K values
+for i, k in enumerate(neighbors):
+	knn = KNeighborsClassifier(n_neighbors=k)
+	knn.fit(X_train, y_train)
+	
+	# Compute training and test data accuracy
+	train_accuracy[i] = knn.score(X_train, y_train)
+	test_accuracy[i] = knn.score(X_test, y_test)
 pred = knn.predict(X_test) 
-print(confusion_matrix(y_test, pred)) 
-print(classification_report(y_test, pred)) 
-error_rate = [] 
-for i in range(1, 40): 
-    knn = KNeighborsClassifier(n_neighbors=i) 
-    knn.fit(X_train, y_train) 
-    pred_i = knn.predict(X_test) 
-    error_rate.append(np.mean(pred_i != y_test)) 
-plt.figure(figsize=(10, 6)) 
-plt.plot(range(1, 40), error_rate, color='blue', linestyle='dashed', marker='o', markerfacecolor='red', 
-markersize=10) 
-plt.title('Error Rate vs. K Value') 
-plt.xlabel('K') 
-plt.ylabel('Error Rate') 
-plt.show() 
+print(confusion_matrix(y_test, pred))
 
+# Generate plot
+plt.plot(neighbors, test_accuracy, label = 'Testing dataset Accuracy')
+plt.plot(neighbors, train_accuracy, label = 'Training dataset Accuracy')
 
-# [[22 3]
-#  [2 3]]
-#     pre     recall  f1-sc   support
-# 0   0.92    0.88    0.90    25
-# 1   0.50    0.60    0.55    5
-
+plt.legend()
+plt.xlabel('n_neighbors')
+plt.ylabel('Accuracy')
+plt.show()
